e5100e4c25ceb8157f7a151422d4a668bcefbd13
fluke@lzy3me <26509248+lzy3me@users.noreply.github.com>
2021-09-13 01:19:24 +0700

    Init Commit. Hotter than today UAT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'case1';
+
+  @ViewChild('selector') function_selector!: ElementRef;
+  @ViewChild('number_in') n!: ElementRef;
+  isFunction = false;
+
+  ngAfterViewInit() {
+    let num = this.n.nativeElement.value;
+    if (num < 1) this.n.nativeElement.value = 1;
+  }
+
+  onChange() {
+    let num = this.n.nativeElement.value;
+    if (num < 1) this.n.nativeElement.value = 1;
+
+    console.log(num);
+    this.isFunction = this.onCalculation(num);
+  }
+
+  onCalculation(num: number) {
+    let f = this.function_selector.nativeElement.value;
+    num = Math.round(num);
+
+    switch (f) {
+      case 'is_prime':
+        for (let i = 2; i < num; i++)
+          if (num % i === 0) return false;
+        return this.isFunction = num > 1;
+      case 'is_fibonacci':
+        const isSqt = (n: number) => {
+          return n > 0 && Math.sqrt(n) % 1 === 0;
+        };
+
+        return isSqt(5 * (num * num) - 4) || isSqt(5 * (num * num) + 4);
+      default:
+        return false;
+    }
+  }
 }
